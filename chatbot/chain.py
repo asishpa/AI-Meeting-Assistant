987ff5d32bac226c241f8f6e869cd5843dbe0c0d
asish <asish@prepairo.dev>
2025-09-28 16:01:39 +0530

    fix

--- chatbot/chain.py
+++ chatbot/chain.py
@@ -12,9 +12,10 @@ model = ChatGoogleGenerativeAI(model="gemini-2.5-flash",streaming=True)
 
 def get_meeting_qa_chain(meeting_id: str):
     retriever = get_retriever(meeting_id)
+    combine_docs_chain = create_stuff_documents_chain(model, prompt)
 
-    combine_docs_chain = create_stuff_documents_chain(model,prompt)
-    return retriever | combine_docs_chain
-
-
-
+    return (
+        retriever
+        | (lambda docs: {"context": docs})   # wrap retriever output into dict
+        | combine_docs_chain
+    )
